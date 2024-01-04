d1c52eb25ee482bbb319caffdafba9fb968e008a
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-04 12:12:37 +0530

    Basic Config

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,18 +1,12 @@
 import { test, expect } from '@playwright/test';
 
+require('dotenv').config();
+
+const baseUrl = process.env.BASE_URL || 'https://onlinelibrary.wiley.com/'
+const SEARCHBOX = "//input[@id='searchField1']";
+
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(baseUrl);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.locator(SEARCHBOX).fill("Hello World", { timeout: 1000 });
 });
