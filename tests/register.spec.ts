f34e78fbd5c3d111b9aab6851d62fc3e18f22743
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-05 10:50:16 +0530

    Advanced Option Part 02

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,11 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-const baseUrl = process.env.BASE_URL || 'https://onlinelibrary.wiley.com/'
-const LOGIN_REGISTER = 'Log in or Register'
-const EMAIL = 'A one-time confirmation email'
-const RETYPE_EMAIL = 'Retype email*'
+const baseUrl = process.env.BASE_URL || 'https://onlinelibrary.wiley.com/';
+const LOGIN_REGISTER = 'Log in or Register';
+const EMAIL = 'A one-time confirmation email';
+const RETYPE_EMAIL = 'Retype email*';
+const PASSWORD = 'Type your password';
+const RETYPE_PASSWORD = 'Re-type your password';
+const FIRST_NAME = 'First Name*';
+const LAST_NAME = 'Last Name*';
+const COUNTRY_LOCATION = 'Country/Location*';
+const AREA_OF_INTEREST = 'Area of interest*';
+const NEWSLETTER = 'Yes, please sign me up for';
+const ACCEPT_TERMS = 'I have read and accept the';
+const NOT_A_ROBOT = 'iframe[name="a-ve19zp2h3y3q"]'
 
-test('Register an Account Positive Test Cases', async ({ page }) => {
+test.only('Register an Account Positive Test Cases', async ({ page }) => {
   await page.goto(baseUrl);
   await page.getByLabel(LOGIN_REGISTER).click();
   await page.getByRole('link', { name: 'NEW USER' }, ).click();
@@ -13,18 +22,39 @@ test('Register an Account Positive Test Cases', async ({ page }) => {
   await page.getByLabel(EMAIL).fill('dimuthchathu@gmail.com');
   await page.getByLabel(RETYPE_EMAIL).click();
   await page.getByLabel(RETYPE_EMAIL).fill('dimuthchathu@gmail.com');
-  await page.getByPlaceholder('Type your password', { exact: true }).click();
-  await page.getByPlaceholder('Type your password', { exact: true }).fill('##Abcd12344');
-  await page.getByPlaceholder('Re-type your password').click();
-  await page.getByPlaceholder('Re-type your password').fill('##Abcd12344');
-  await page.getByLabel('First Name*').click();
-  await page.getByLabel('First Name*').fill('DimuthC');
-  await page.getByLabel('Last Name*').click();
-  await page.getByLabel('Last Name*').fill('Bandara');
-  await page.getByLabel('Country/Location*').selectOption('country-lk');
-  await page.getByLabel('Area of interest*').selectOption('psychiatry');
-  await page.getByText('Yes, please sign me up for').click();
-  await page.getByText('I have read and accept the').click();
-  await page.frameLocator('iframe[name="a-ve19zp2h3y3q"]').getByLabel('I\'m not a robot').click();
+  await page.getByPlaceholder(PASSWORD, { exact: true }).click();
+  await page.getByPlaceholder(PASSWORD, { exact: true }).fill('##Abcd12344');
+  await page.getByPlaceholder(RETYPE_PASSWORD).click();
+  await page.getByPlaceholder(RETYPE_PASSWORD).fill('##Abcd12344');
+  await page.getByLabel(FIRST_NAME).click();
+  await page.getByLabel(FIRST_NAME).fill('DimuthC');
+  await page.getByLabel(LAST_NAME).click();
+  await page.getByLabel(LAST_NAME).fill('Bandara');
+  await page.getByLabel(COUNTRY_LOCATION).selectOption('country-lk');
+  await page.getByLabel(AREA_OF_INTEREST).selectOption('psychiatry');
+  await page.getByText(NEWSLETTER).click();
+  await page.getByText(ACCEPT_TERMS).click();
+  await page.frameLocator(NOT_A_ROBOT).getByLabel('I\'m not a robot').click();
+  await page.getByRole('button', { name: 'REGISTER', exact: true }).click();
+});
+
+test('Register an Account Negative Test Cases', async ({ page }) => {
+  await page.goto(baseUrl);
+  await page.getByLabel(LOGIN_REGISTER).click();
+  await page.getByRole('link', { name: 'NEW USER' }, ).click();
+  await page.getByLabel(EMAIL).click();
+  await page.getByLabel(EMAIL).fill('dimuthchathu');
+  await page.getByLabel(RETYPE_EMAIL).click();
+  await page.getByLabel(RETYPE_EMAIL).fill('dimuthchathu@');
+  await page.getByPlaceholder(PASSWORD, { exact: true }).click();
+  await page.getByPlaceholder(PASSWORD, { exact: true }).fill('##Abc');
+  await page.getByPlaceholder(RETYPE_PASSWORD).click();
+  await page.getByPlaceholder(RETYPE_PASSWORD).fill('##Abcd');
+  await page.getByLabel(FIRST_NAME).click();
+  await page.getByLabel(FIRST_NAME).fill('DimuthC');
+  await page.getByLabel(LAST_NAME).click();
+  await page.getByLabel(LAST_NAME).fill('Bandara');
+  await page.getByText(NEWSLETTER).click();
+  await page.getByText(ACCEPT_TERMS).click();
   await page.getByRole('button', { name: 'REGISTER', exact: true }).click();
 });
