5468b8743b5a27e43cf46c11a40b44e37a85a41d
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-06 09:26:54 +0530

    Search Options New

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -9,10 +9,23 @@ const SEARCHBOX = "//input[@id='searchField1']";
 test('Advanced Search Option ', async ({ page }) => {
   await page.goto(baseUrl);
   await page.locator(SEARCHBOX).fill("Information Technology", { timeout: 1000 });
-  await page.goto('https://onlinelibrary.wiley.com/action/doSearch?AllField=Information+Technology');
   
-  // Displaying Search Results 
+  await page.goto('https://onlinelibrary.wiley.com/action/doSearch?AllField=Information+Technology');
   await page.locator('li').filter({ hasText: 'Special Section: Information Policy Free Access free What about reader privacy' }).getByRole('link').nth(4).click();
-  await page.locator('.meta__authors').first().click();
   await page.getByRole('link', { name: 'What about reader privacy?' }).click();
+  await page.getByRole('button', { name: 'refine search' }).click();
+  await page.getByPlaceholder('Enter a journal, book, or').click();
+  await page.getByPlaceholder('Enter a journal, book, or').fill('Wiley');
+  await page.getByRole('button', { name: 'Search', exact: true }).click();
+  await page.goto('https://onlinelibrary.wiley.com/action/doSearch?AllField=Information+Technology#');
+  await page.getByRole('button', { name: 'refine search' }).click();
+  await page.getByRole('tab', { name: 'Search History' }).click();
+  await page.getByRole('columnheader', { name: 'Search name' }).click();
+  await page.getByRole('cell', { name: 'All: information technology (2293006)', exact: true }).click();
+  await page.getByRole('columnheader', { name: 'Searched On' }).click();
+  await page.getByRole('cell', { name: 'Jan 05' }).click();
+  await page.getByRole('link', { name: 'Publications (360)' }).click();
+  
+  
 });
+
